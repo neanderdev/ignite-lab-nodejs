8a3e35e48433272f600031758d37c75a92d74135
Neander de Souza <neander.faria@hotmail.com>
2023-02-08 14:14:55 -0300

    Aula 01 �

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+
+import { PrismaService } from './prisma.service';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [PrismaService],
 })
 export class AppModule {}
